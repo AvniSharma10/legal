49adc18d193eff919aa06c7f4a6791570856081f
Avni Sharma <117329971+AvniSharma10@users.noreply.github.com>
2024-07-20 20:47:41 +0530

    Added files

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
 import express from "express";
+import bodyParser from "body-parser";
+
+
 
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
@@ -13,8 +20,15 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
+  res.render("contact", { message: null });
 });
+
+app.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const message = `Thank you, ${name}, for the comment.`;
+  res.render("contact.ejs", { message });
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
